3e080275435485ee1a984e5c4778af688ebea305
Anwesh43 <anweshthecool0@gmail.com>
2018-03-23 09:34:28 +0530

    adding BallRotrView in activity

--- app/src/main/java/ui/anwesome/com/kotlinballrotrview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinballrotrview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinballrotrview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.ballrotrview.BallRotrView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BallRotrView.create(this)
     }
 }
