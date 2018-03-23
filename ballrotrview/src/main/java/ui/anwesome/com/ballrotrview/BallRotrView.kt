7bc198815fa0ca4b7bcda4f0d9b3dd17edec6c4c
Anwesh43 <anweshthecool0@gmail.com>
2018-03-23 08:58:02 +0530

    adding State class

--- ballrotrview/src/main/java/ui/anwesome/com/ballrotrview/BallRotrView.kt
+++ ballrotrview/src/main/java/ui/anwesome/com/ballrotrview/BallRotrView.kt
@@ -14,9 +14,31 @@ class BallRotrView(ctx : Context) : View(ctx) {
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+    data class State(var prevScale : Float = 0f, var dir : Int = 0, var j : Int = 0) {
+        val scales : Array<Float> = arrayOf(0f, 0f, 0f, 0f, 0f)
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += dir * 0.1f
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                scales[j] = prevScale + dir
+                j += dir
+                if (j == scales.size || j == -1) {
+                    j -= dir
+                    scales[j] = prevScale + dir
+                    dir = 0
+                    stopcb(prevScale)
+                }
+            }
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0) {
+                dir = 1 - 2 * prevScale.toInt()
+                startcb()
+            }
+        }
     }
 }
