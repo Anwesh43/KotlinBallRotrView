0d000b70a75f8e4bb4a6052eb6d002794deeb1e0
Anwesh43 <anweshthecool0@gmail.com>
2018-03-24 08:34:09 +0530

    adding fullScreen support

--- app/src/main/java/ui/anwesome/com/kotlinballrotrview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinballrotrview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlinballrotrview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.ballrotrview.BallRotrView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BallRotrView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
