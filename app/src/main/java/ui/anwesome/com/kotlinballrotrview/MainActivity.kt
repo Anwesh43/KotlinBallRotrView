32e5bf1f4d865085ccf8718b5fe657281a7390d3
Anwesh43 <anweshthecool0@gmail.com>
2018-03-24 08:44:06 +0530

    adding OnMovementCompleteListener in activity

--- app/src/main/java/ui/anwesome/com/kotlinballrotrview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinballrotrview/MainActivity.kt
@@ -3,13 +3,17 @@ package ui.anwesome.com.kotlinballrotrview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import ui.anwesome.com.ballrotrview.BallRotrView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BallRotrView.create(this)
+        val view = BallRotrView.create(this)
+        view.addOnCompleteListener {
+            Toast.makeText(this, "complete", Toast.LENGTH_SHORT).show()
+        }
         fullScreen()
     }
 }
